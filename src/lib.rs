afabc3569658c26ab8ce1819f332e91f96c70685
LOU Xun <xun.lou@controlant.com>
2023-09-28 12:03:20 +0000

    Add assume role utility

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 //! Utilities for AWS controllers
 
 pub mod org;
+
+use aws_config::SdkConfig;
+use aws_types::region::Region;
+use std::sync::Arc;
+
+/// Build `SdkConfig` by assuming a role and in the specified region
+pub async fn assume_role(role: impl Into<String>, region: Region) -> SdkConfig {
+  use aws_config::{default_provider::credentials::DefaultCredentialsChain, sts::AssumeRoleProvider};
+
+  let provider = AssumeRoleProvider::builder(role)
+    .session_name(env!("CARGO_PKG_NAME"))
+    .region(region.clone())
+    .build(Arc::new(DefaultCredentialsChain::builder().build().await) as Arc<_>);
+
+  aws_config::from_env()
+    .credentials_provider(provider)
+    .region(region)
+    .load()
+    .await
+}
